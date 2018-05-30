5d3b59071a2c6db4b8305c3ed98493c6f421166e
Trần minh tuấn <tuan260387@gmail.com>
2018-05-30 15:49:17 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, {Component} from 'react';
+import { Navbar, NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, NavLink } from 'mdbreact';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Routes from './Routes';
+import FooterMain from './Componet/Footer/Footer';
+import mainLogo from './img/logo.gif';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import './css/mdb.css';
+import './App.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            collapsed: false,
+        };
+        this.handleTogglerClick = this.handleTogglerClick.bind(this);
+        this.handleNavbarClick = this.handleNavbarClick.bind(this);
+
+    }
+
+    handleTogglerClick(){
+        this.setState({
+            collapsed: !this.state.collapsed
+        });
+    }
+
+    handleNavbarClick(){
+        this.setState({
+            collapsed: false
+        });
+    }
+
+    render() {
+        const collapsed = this.state.collapsed;
+        const overlay = <div id="sidenav-overlay" style={{backgroundColor: 'transparent', color: "#fff"}} onClick={this.handleNavbarClick}/>;
+
+        return (
+            <Router>
+                <div className="flyout">
+                    <Navbar className="nav-main" expand="md" fixed="top" scrolling>
+                        <NavbarBrand href="/">
+                            <img src={ mainLogo } alt="or-trans" height="32" /> OR - TRANS
+                        </NavbarBrand>
+                        <NavbarToggler onClick={this.handleTogglerClick}/>
+                        <Collapse isOpen={this.state.collapsed} navbar>
+                            <NavbarNav right onClick={this.handleNavbarClick}>
+                                <NavItem>
+                                    <NavLink to="/">Home</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink to="/signin">Sign In</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink to="/css">CSS</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink to="/components">Components</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink to="/advanced">Advanced</NavLink>
+                                </NavItem>
+                            </NavbarNav>
+                        </Collapse>
+                    </Navbar>
+                    { collapsed && overlay}
+                    <main style={{marginTop: '4rem'}}>
+                        <Routes />
+                    </main>
+                    <FooterMain />
+                </div>
+            </Router>
+        );
+    }
+}
+
+export default App;
---
+++ src/Componet/Contact/SignIn.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import IntlTelInput from 'react-intl-tel-input';
+import libphonenumber from '../../../node_modules/react-intl-tel-input/dist/libphonenumber.js';
+import '../../../node_modules/react-intl-tel-input/dist/main.css';
+
+class SimpleSelect extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state ={
+      value: '',
+      ecountryData: '',
+      phone: '0975227856',
+    };
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+  }
+
+
+
+  onChangeHandler(status, value, countryData, number, id){
+    console.log(status, value, countryData, number, id);
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <IntlTelInput
+          onPhoneNumberChange={ this.onChangeHandler }
+          onPhoneNumberBlur={ this.onChangeHandler }
+          preferredCountries={['vn']}
+          onSelectFlag={null}
+          css={['intl-tel-input', 'form-control']}
+                      utilsScript={libphonenumber} />
+      </div>
+
+    );
+  }
+}
+
+
+export default SimpleSelect;
